2049fe65228e40745beea0865e372e3dc3751690
ppullen <pppullen@gmail.com>
2023-02-02 23:08:04 -0500

    Added scoreboard, and collisions with food and walls

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 """this is declared out side the while loop for some reason
 Apparently this is one of the causes of errors 
@@ -30,4 +34,30 @@ while game_is_on:
 
     snake.move()
 
-# screen.exitonclick()
+    # Detect collision with food
+    # turtle.distance is a function that checks the distance between to entities
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if (snake.head.xcor() > 280) or (snake.head.xcor() < -280) or (snake.head.ycor() > 280) or (snake.head.ycor() < -280):
+        scoreboard.game_over()
+        game_is_on = False
+
+    # Detect collision with tail
+    # removes the if segment is snakehead, and inside slices the out of the list
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # for segment in snake.segments:
+    #     if segment == snake.head:
+    #         pass
+    #     elif snake.head.distance(segment) < 10:
+    #         game_is_on = False
+    #         scoreboard.game_over()
+
+screen.exitonclick()
--- snake.py
+++ snake.py
@@ -18,12 +18,18 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            # penup removes the white line
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        # penup removes the white line
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
     def move(self):
         """ The tail moves first, so that you have the
